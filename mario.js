bae5abfe39ba7c5c6cec06d5d72cacf46f6a6522
Candice Brown <CandiceBrown97@gmail.com>
2017-03-27 19:02:34 -0400

    completed final tweaks on Mario2 and submitted code for approval

--- mario.js
+++ mario.js
@@ -8,10 +8,12 @@ drawPyramid(5);
  * Renders, in the HTML document, a Mario pyramid of the specified height
  */
 function drawPyramid(height) {
+	//so first I must create a container for the <div id> pyramid
+	var obtainTheContainer = document.getElementById("pyramid");
 
-    // TODO 2
-    // delete the "under construction" element, ie the <div id="notice">
-
+	//delete construction p tag sentences programmatically
+	var deleteConstructionDiv = document.getElementById("construction");
+	obtainTheContainer.removeChild(deleteConstructionDiv);
 
     // for each row....
     for (var row = 0; row < height; row++) {
@@ -30,11 +32,43 @@ function drawPyramid(height) {
         }
 
         // you can delete this now
-        console.log(rowStr)
+        // console.log(rowStr)
 
-        // TODO 1
+				//Add variable rowStr (containing string for current row) to the document
+
+
+				//var obtainTheContainer = document.getElementById("pyramid");
+				//Create a <p> element
+				var theParagraphElement = document.createElement("p");
+
+				//set p element's text to be the string
+				//restated: and set it's text to be the string of the p element
+				theParagraphElement.innerHTML = rowStr;
+				obtainTheContainer.appendChild(theParagraphElement);
+
+		}
+}
+			// 	//Insert the <p> (paragraph element) as a child of the container <div>
+			// 	//so first I must create a container for the <div>
+			// var obtainTheContainer = document.getElementById("pyramid");
+				//obtainTheContainer.appendChild(theParagraphElement);
+
+				//Delete the "under construction" notice from the container <div>
+				//from the Mario2 file:
+						// <div id="construction">
+		        //     <p>Uh oh! the pyramid is still under construction.</p>
+		        //     <p>Check back again soon... our developers are hard at wrk as we speak!</p>
+		  //       // </div>
+			// var deleteConstructionDiv = document.getElementById("construction");
+			// obtainTheContainer.removeChild(deleteConstructionDiv);
+
+
         // create an element whose inner text is rowStr,
         // and insert it as a child of the container <div id="pyramid">
+				//from the Mario2 file:
+				// <div id="pyramid">
+        // <!-- Our pyramid will go inside here -->
+				//</div>
 
-    }
-}
+				//create element for housing pyramid
+				//var pyramidContainer = document.getElementById("pyramid");
